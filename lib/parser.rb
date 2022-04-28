9477ef4454c34b277bfa2d100d5a8ed8520b789e
Philip <philipmjobe@gmail.com>
2022-04-28 16:32:24 -0400

    gets favorite color and date of birth to print as well

--- lib/parser.rb
+++ lib/parser.rb
@@ -35,7 +35,7 @@ def parse_comma_delimited
 end
 
 def print_person(person)
-  puts "#{person[:last_name]} #{person[:first_name]} #{person[:middle_initial]} #{person[:gender]}"
+  puts "#{person[:last_name]} #{person[:first_name]} #{person[:middle_initial]} #{person[:gender]} #{person[:date_of_birth]} #{person[:favorite_color]}"
 end
 
 [
