346f19d7cea5bde97eb30d11f119f60c830c8f2d
Philip <philipmjobe@gmail.com>
2022-05-09 11:54:55 -0400

    gets everthing working as well as I could

--- lib/comma_parser.rb
+++ lib/comma_parser.rb
@@ -12,14 +12,17 @@ class CommaParser
     @file = CSV.read(file_path, col_sep: DELIMITER)
   end
 
+  
   def parse 
+    file.map! { |row| row[4] = Date.strptime(row[4], "%m/%d/%Y"); row }
+    file.sort_by! { |row| row[4] }
     file.map do |row|
       Person.new(
         last_name: row[0],
         first_name: row[1],
         gender: row[2],
         favorite_color: row[3],
-        date_of_birth: Date.strptime(row[4], "%m/%d/%Y").strftime("%_m/%-d/%Y")
+        date_of_birth: row[4].strftime("%-m/%-d/%Y")
       )
     end
   end
--- lib/parser.rb
+++ lib/parser.rb
@@ -3,6 +3,14 @@ require_relative "space_parser"
 require_relative "comma_parser"
 
 puts "Welcome to the file parser"
+puts
+puts "I am going to show you something amazing! Would you like to see?"
+response = gets.chomp 
+
+if response == "No"
+  puts "Fine be that way."
+  clear 
+end 
 
 def parse_pipe_delimited
   PipeParser.new("assets/pipe.txt").parse
@@ -29,11 +37,21 @@ def print_person(person)
   ].values_at(0, 1, 3, 4, 5).compact.join(" "))
 end
 
+
 [
   parse_pipe_delimited,
   parse_space_delimited,
   parse_comma_delimited
 ].flatten.sort{ |a, b| [a.gender, a.last_name] <=> [b.gender, b.last_name] }.map do |person|
+  print_person(person)
+ end
+
+puts 
+[
+  parse_pipe_delimited,
+  parse_space_delimited,
+  parse_comma_delimited
+].flatten.sort{ |a, b| [a.date_of_birth, a.last_name] <=> [b.date_of_birth, b.last_name] }.map do |person|
   print_person(person)
 end
 puts 
@@ -41,6 +59,6 @@ puts
   parse_pipe_delimited,
   parse_space_delimited,
   parse_comma_delimited
-].flatten.sort{ |a, b| [a.date_of_birth, a.last_name] <=> [b.date_of_birth, b.last_name] }.map do |person|
+].flatten.sort{ |a, b| [a.last_name] <=> [b.last_name] }.reverse.map do |person|
   print_person(person)
 end
